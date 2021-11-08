bf5590a397e9669526ea08d5d2a9c46179129d5e
hearnderek <hearnderek@yahoo.com>
2021-11-09 02:11:48 +0900

    partials (aka incorrect)

--- RemoveDuplicateLetters.py
+++ RemoveDuplicateLetters.py
@@ -1,6 +1,45 @@
+from dataclasses import dataclass
+
+@dataclass
+class BestRemoval:
+    s: str
+    c: str
+    i: int
 
 
 class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        
+        cfs = dict()
+        i = 0
+        for c in s:
+            if c not in cfs:
+                cfs[c] = 1
+            else:
+                cfs[c] += 1
+            i += 1
+        
+        while any(x > 1 for x in cfs.values()):
+            br = None
+
+            fc = None
+            for (i, c) in enumerate(s):
+                if cfs[c] == 1:
+                    continue
+                else:
+                    fc = c
+                    break
+            for (i, c) in enumerate(s):
+                if cfs[c] == 1:
+                    continue
+                sr = s[0:i] + s[i+1:]
+                if not br or br.s > sr:
+                    br = BestRemoval(sr, c, i)
+            cfs[br.c] -= 1
+            s = br.s
+            print(s)
+        return s
+
     def new_removeDuplicateLetters(self, s: str) -> str:
         xs = set(s)
         for c in sorted(xs):
@@ -22,7 +61,7 @@ class Solution:
         pass
 
 
-    def removeDuplicateLetters(self, s: str) -> str:
+    def old_removeDuplicateLetters(self, s: str) -> str:
         """
         My initial solution was incorrect use to a misunderstanding of the problem.
         We are not able to reorder the characters, we can only remove characters.
@@ -63,11 +102,12 @@ if __name__ == "__main__":
     xs = [
         "rusrbofeggbbkyuyjsrzornpdguwzizqszpbicdquakqws",
         # "abacb",
-        # "abacb",
         # "bcabc",
         # "cbacdcbc",
         # "aaaaaaaaaabbbbbbbbbbccccccccccddddddddddeeeeeeeeeeffffffffffgggggggggghhhhhhhhhhiiiiiiiiiijjjjjjjjjjkkkkkkkkkkllllllllllmmmmmmmmmmnnnnnnnnnnooooooooooppppppppppqqqqqqqqqqrrrrrrrrrrssssssssssttttttttttuuuuuuuuuuvvvvvvvvvvwwwwwwwwwwxxxxxxxxxxyyyyyyyyyyzzzzzzzzzz",
         # "aaaaaaaaaabbbbbbbbbbccccccccccddddddddddeeeeeeeeeeffffffffffgggggggggghhhhhhhhhhiiiiiiiiiijjjjjjjjjjkkkkkkkkkkllllllllllmmmmmmmmmmnnnnnnnnnnooooooooooppppppppppqqqqqqqqqqrrrrrrrrrrssssssssssttttttttttuuuuuuuuuuvvvvvvvvvvwwwwwwwwwwxxxxxxxxxxyyyyyyyyyyzzzzzzzzzzaaaaaaaaaabbbbbbbbbbccccccccccddddddddddeeeeeeeeeeffffffffffgggggggggghhhhhhhhhhiiiiiiiiiijjjjjjjjjjkkkkkkkkkkllllllllllmmmmmmmmmmnnnnnnnnnnooooooooooppppppppppqqqqqqqqqqrrrrrrrrrrssssssssssttttttttttuuuuuuuuuuvvvvvvvvvvwwwwwwwwwwxxxxxxxxxxyyyyyyyyyyzzzzzzzzzz"
         ]
     for x in xs:
-        print(Solution().new_removeDuplicateLetters(x))
+        print(Solution().removeDuplicateLetters(x))
+
+    print("bfegkuyjorndiqszpcaw")
